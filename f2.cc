55da99c16763bcdaed46ee31fa37661e10c61ee3
Nika Mansouri Ghiasi <mnika@student.ethz.ch>
2017-01-10 04:13:56 +0330

    Update f2.cc

--- f2.cc
+++ f2.cc
@@ -12,9 +12,9 @@
 #include <unordered_map>
 // #include "rdtsc.h"
 
-#define LINE_GRANULARITY 8000
-//#define LINE_GRANULARITY hongyi num 1000000
-#define CHUNK_SIZE 100
+#define LINE_GRANULARITY 800
+//#define LINE_GRANULARITY hongyi num 1000000- once worked with 10000
+#define CHUNK_SIZE 10
 
 
 using namespace std;
